3910a5dc707516a0d0c0d3d1c91ccb3e5df87ee4
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-02-08 17:59:56 +0100

    Update server.h
    Fixed Address to any

--- server/src/server.h
+++ server/src/server.h
@@ -1,7 +1,7 @@
 #include <netinet/ip.h>
 
 #define PORT 1025
-#define ADDRESS INADDR_LOOPBACK
+#define ADDRESS INADDR_ANY
 #define QUEUE_LENGHT 50
 
 void server_socket_setup(int *socketFD, struct sockaddr_in *socketAddress);
