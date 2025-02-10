c5ac91619300fb7d985c8afcd7730ac82a1d1a0e
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-02-10 13:27:33 +0100

    Update types.h
    Created http_request and http_response types

--- server/src/types.h
+++ server/src/types.h
@@ -1,12 +1,36 @@
-#include <netinet/ip.h>
-
 #ifndef TYPES_H
 #define TYPES_H
+
+#include <netinet/ip.h>
+#include "picohttpparser.h"
 
 struct destination_address {
     int socketFD;
     struct sockaddr_in address;
     socklen_t length;
 };
+
+typedef struct {
+    const char *method;
+    size_t method_len;
+    const char *path;
+    size_t path_len;
+    int minor_version;
+    struct phr_header headers[100];
+    size_t num_headers;
+    const char *body;
+    size_t body_len;
+} http_request;
+
+typedef struct {
+    int status_code;
+    const char *status_phrase;
+    size_t status_phrase_len;
+    int minor_version;
+    struct phr_header headers[100];
+    size_t num_headers;
+    const char *body;
+    size_t body_len;
+} http_response;
 
 #endif
