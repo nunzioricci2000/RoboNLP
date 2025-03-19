805806cc6d81cb47c4a8233e269fa41bd6589616
nunzioricci2000 <113517407+nunzioricci2000@users.noreply.github.com>
2025-03-19 09:28:49 +0100

    Update __main__.py
    setted dependencies

--- client/app/__main__.py
+++ client/app/__main__.py
@@ -1,6 +1,11 @@
 from control import Controller
+from robot_connection import FurhatConnection
+from server_connection import ServerConnection
 
-controller = Controller()
+controller = Controller(
+    robot=FurhatConnection(),
+    server=ServerConnection(),
+    )
 
 controller.login()
 controller.chat()
